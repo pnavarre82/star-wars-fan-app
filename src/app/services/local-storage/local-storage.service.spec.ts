bc739440a49bbb8d7b8403495965112e15eedc87
Pedro Andres <pedroandresblanco@gmail.com>
2018-11-24 16:52:37 +0100

    LocalStoregeService to store persistent data using localStorage

--- src/app/services/local-storage/local-storage.service.spec.ts
+++ src/app/services/local-storage/local-storage.service.spec.ts
@@ -1,26 +1,45 @@
 import { TestBed } from '@angular/core/testing';
+import * as faker from 'faker';
 
 import { LocalStorageService } from './local-storage.service';
+import { Injectable } from '@angular/core';
 
 describe('LocalstorageserviceService', () => {
+  let service: LocalStorageService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    let store: { [key: string]: string } = {};
-
-    // prevent using real localStorage
-    spyOn(localStorage, 'getItem').and.callFake(function(key) {
-      return store[key];
-    });
-    spyOn(localStorage, 'setItem').and.callFake(function(key, value) {
-      return (store[key] = value + '');
-    });
-    spyOn(localStorage, 'clear').and.callFake(function() {
-      store = {};
-    });
+    service = TestBed.get(LocalStorageService);
+    localStorage.clear();
   });
 
   it('should be created', () => {
-    const service: LocalStorageService = TestBed.get(LocalStorageService);
     expect(service).toBeTruthy();
   });
+
+  it('should getItem return undefined if key not set', () => {
+    expect(service.getItem(faker.random.word())).toBeUndefined();
+  });
+
+  it('should getItem expected object if setItem previously invoked (also parse dates)', () => {
+    const key = faker.random.word();
+    const fakeObject = faker.helpers.createCard();
+    service.setItem(key, fakeObject);
+    const returnedObject = service.getItem(key);
+    expect(returnedObject).toEqual(fakeObject);
+  });
 });
+
+@Injectable()
+export class MockLocalStorageService {
+  store: { [key: string]: any } = {};
+
+  constructor() {}
+  setItem(key: string, value: any) {
+    this.store[key] = value;
+  }
+
+  getItem(key: string): any {
+    return this.store[key];
+  }
+}
--- src/app/services/local-storage/local-storage.service.ts
+++ src/app/services/local-storage/local-storage.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+const jsonDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+
 /**
  * Wrap localStorage dependency
  *
@@ -11,4 +13,21 @@ import { Injectable } from '@angular/core';
 })
 export class LocalStorageService {
   constructor() {}
+
+  setItem(key: string, value: any) {
+    window.localStorage[key] = JSON.stringify(value);
+  }
+
+  getItem(key: string): any {
+    const storagedString = window.localStorage[key];
+    return storagedString == null ? undefined : JSON.parse(storagedString, this.dateReviver);
+  }
+
+  private dateReviver(key, value) {
+    if (typeof value === 'string' && jsonDateFormat.test(value)) {
+      return new Date(value);
+    }
+
+    return value;
+  }
 }
