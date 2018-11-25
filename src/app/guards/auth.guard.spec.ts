546be10dfbefec3934a56ecdfccfde6934a83663
Pedro Andres <pedroandresblanco@gmail.com>
2018-11-25 12:55:01 +0100

    AuthGuard redirects if can't activate.  closes #4

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -4,8 +4,14 @@ import { AuthGuard } from './auth.guard';
 import { AuthService } from '../services/auth/authservice.service';
 import { MockAuthService } from '../services/auth/authservice.service.spec';
 import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { RoutesPaths } from '../app-routing-paths.class';
 
 describe('AuthGuard', () => {
+  class MockRouterService {
+    navigate() {}
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -13,28 +19,35 @@ describe('AuthGuard', () => {
         {
           provide: AuthService,
           useClass: MockAuthService
+        },
+        {
+          provide: Router,
+          useClass: MockRouterService
         }
       ]
     });
   });
 
-  it('should canActivate return true if isLoged return true', inject(
-    [AuthGuard, AuthService],
-    (guard: AuthGuard, authService: MockAuthService) => {
+  it('should canActivate return true if isLoged return true', done => {
+    inject([AuthGuard, AuthService], (guard: AuthGuard, authService: MockAuthService) => {
       spyOn(authService, 'isLogged').and.returnValue(of(true));
       guard.canActivate().subscribe(result => {
         expect(result).toBeTruthy();
+        done();
       });
-    }
-  ));
+    })();
+  });
 
-  it('should canActivate return false if isLoged return false', inject(
-    [AuthGuard, AuthService],
-    (guard: AuthGuard, authService: MockAuthService) => {
+  it('should canActivate return false and navigate to /login if isLoged return false', done => {
+    inject([AuthGuard, AuthService], (guard: AuthGuard, authService: MockAuthService) => {
+      const mockRouter: MockRouterService = TestBed.get(Router);
       spyOn(authService, 'isLogged').and.returnValue(of(false));
+      spyOn(mockRouter, 'navigate');
       guard.canActivate().subscribe(result => {
         expect(result).toBeFalsy();
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/' + RoutesPaths.Loging]);
+        done();
       });
-    }
-  ));
+    })();
+  });
 });
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth/authservice.service';
+import { tap } from 'rxjs/operators';
+import { RoutesPaths } from '../app-routing-paths.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(): Observable<boolean> {
-    return this.authService.isLogged();
+    return this.authService.isLogged().pipe(
+      tap(result => {
+        if (!result) {
+          this.router.navigate(['/' + RoutesPaths.Loging]);
+        }
+      })
+    );
   }
 }
